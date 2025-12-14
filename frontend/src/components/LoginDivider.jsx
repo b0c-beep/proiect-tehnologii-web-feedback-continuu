e578d7515f748bad479eb870884fcacef147443f
b0c-beep <bocmarioadrian98@gmail.com>
2025-12-14 23:28:36 +0200

    added form component to content

---
+++ frontend/src/components/LoginDivider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@styles/LoginDivider.css";
+
+const LoginDivider = () => {
+    return (
+        <div className="styled-divider">
+            <div className="divider-line" />
+            <span className="divider-label">OR</span>
+            <div className="divider-line" />
+        </div>
+    );
+};
+
+export default LoginDivider;
---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "@styles/LoginForm.css";
+import LoginButton from "@components/LoginButton";
+import LoginDivider from "@components/LoginDivider";
+import { clickJoinSession, clickAccessDashboard, clickRegister } from "@utils/login_api.js";
+import { useState } from "react";
+
+const LoginForm = ({ role }) => {
+    const [accessCode, setAccessCode] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [regEmail, setRegEmail] = useState("");
+    const [regPassword, setRegPassword] = useState("");
+    
+    const handleJoinSession = (e) => {
+        e.preventDefault();
+        clickJoinSession(accessCode);
+    }
+
+    const handleAccessDashboard = (e) => {
+        e.preventDefault();
+        clickAccessDashboard(email, password);
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        clickRegister(regEmail, regPassword);
+    }
+
+    return (
+        <div className="form-inputs">
+            {role === 'student' ? (
+                <>
+                    <div className="input-group">
+                        <label className="input-label">Access Code</label>
+                        <input type="text" placeholder="CODE" className="input-field text-center" 
+                        value={accessCode} onChange={(e) => setAccessCode(e.target.value)} />
+                    </div>
+
+                    <LoginButton text="Join Session" clickHandler={handleJoinSession} />
+                </>
+            ) : (
+                <>
+                    <div className="input-group">
+                        <label className="input-label">Email</label>
+                        <input type="email" placeholder="professor@university.edu" className="input-field" 
+                        value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="input-group">
+                        <label className="input-label">Password</label>
+                        <input type="password" placeholder="Password" className="input-field" 
+                        value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+
+                    <LoginButton text="Access Dashboard" clickHandler={handleAccessDashboard} />
+
+                    <LoginDivider />
+
+                    <div className="input-group">
+                        <label className="input-label">First Name</label>
+                        <input type="text" placeholder="First Name" className="input-field" 
+                        value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    </div>
+                    <div className="input-group">
+                        <label className="input-label">Last Name</label>
+                        <input type="text" placeholder="Last Name" className="input-field" 
+                        value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    </div>
+                    <div className="input-group">
+                        <label className="input-label">Email</label>
+                        <input type="email" placeholder="professor@university.edu" className="input-field" 
+                        value={regEmail} onChange={(e) => setRegEmail(e.target.value)} />
+                    </div>
+                    <div className="input-group">
+                        <label className="input-label">Password</label>
+                        <input type="password" placeholder="Password" className="input-field" 
+                        value={regPassword} onChange={(e) => setRegPassword(e.target.value)} />
+                    </div>
+
+                    <LoginButton text="Register" clickHandler={handleRegister} />
+
+                </>
+            )}
+        </div>
+    );
+};
+
+export default LoginForm;
